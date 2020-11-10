1b2be6ef2b750120cf1a1704bea9a678a15f72d4
nikolaichub <chub.mykola@ukr.net>
2020-11-10 12:05:24 +0200

    arrays.c was supplemented

--- Algorithms and Data Structures/Practice/arrays.c
+++ Algorithms and Data Structures/Practice/arrays.c
@@ -5,15 +5,23 @@
 
 int main() {
 
-    int array[SIZE] = {0};
+    // basic operations with arrays
 
-    for (int i = 0; i <= SIZE/2; ++i) {
-        array[2*i] = i;
-        array[2 * i + 1] = SIZE/2 + i;
+    int array1[SIZE] = {0};
+    int array2[SIZE] = {0};
+    int array3[SIZE] = {0};
+
+    for (int i = 0; i < SIZE; ++i) array1[i] = i; // 0 1 2 3 4 5 6 7 8 9
+
+    for (int i = 0; i < SIZE; ++i) array2[i] = SIZE - i - 1; // 9 8 7 6 5 4 3 2 1 0
+
+    for (int i = 0; i < SIZE/2; ++i) {
+        array3[2*i] = i;
+        array3[2 * i + 1] = SIZE/2 + i; // 0 5 1 6 2 7 3 8 4 9
     };  
 
     for (int i = 0; i < SIZE; ++i) 
-        printf(" %d ", array[i]);
+        printf("%d ", array2[i]); 
 
     return 0;
 }

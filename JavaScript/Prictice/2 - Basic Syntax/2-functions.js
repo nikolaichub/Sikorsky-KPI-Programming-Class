5211ea1b9955c16bc07ed3fa82c40bd3f7779ee2
nikolaichub <chub.mykola@ukr.net>
2020-11-17 12:23:01 +0200

    2-functions.js finished

--- JavaScript/Prictice/2 - Basic Syntax/2-functions.js
+++ JavaScript/Prictice/2 - Basic Syntax/2-functions.js
@@ -9,6 +9,20 @@ const avarage = (a, b) => (a + b) / 2;
 const square = a => a*a;
 const cube = a => a*a*a;
 
-console.log(avarage(a,b));
-console.log(square(a));
-console.log(cube(a));
+console.log(`avarage between ${a} and ${b}:`,avarage(a,b));
+console.log(`square of ${a}:`,square(a));
+console.log(`cube of ${a}:`,cube(a));
+
+const calculate = () => {
+    let array = [];
+    for (let i = 0; i <= 9; i++) {
+        let iSquare = square(i);
+        let iCube = cube(i); 
+        let iAvarage = avarage(iSquare, iCube); 
+        array.push(iAvarage);
+    }
+
+    return array;
+}
+
+console.log(calculate());

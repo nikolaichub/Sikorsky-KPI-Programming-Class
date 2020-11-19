a7de4bba7ddedbd8881809acd8ffca721874ecbd
nikolaichub <chub.mykola@ukr.net>
2020-11-19 23:59:30 +0200

    formula of generation random integers changed

--- JavaScript/Prictice/3 - Functions and Methods/0-random.js
+++ JavaScript/Prictice/3 - Functions and Methods/0-random.js
@@ -3,7 +3,7 @@
 // gets random integer
 const getRandom = (min, max) => {
     //return Math.floor(Math.random() * max + 1); //works in lack of min value
-    return Math.floor(Math.random() * (max - min) + min + 1);
+    return Math.floor(Math.random() * (max - min) + min);
 
 };
 

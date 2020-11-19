7a4c34a0b7e87314ffe4cedead017687c67f20c9
nikolaichub <chub.mykola@ukr.net>
2020-11-19 23:27:29 +0200

    simplified and can be exported

--- JavaScript/Prictice/3 - Functions and Methods/0-random.js
+++ JavaScript/Prictice/3 - Functions and Methods/0-random.js
@@ -1,19 +1,10 @@
 'use strict';
 
-
-/*const getRandom = (min, max) => {
+// gets random integer
+const getRandom = (min, max) => {
     //return Math.floor(Math.random() * max + 1); //works in lack of min value
-}*/
-
-/* function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min + 1);
-}*/
 
-let times = 20;
-const randomMultiOutput = []
+};
 
-// for lazy output
-for (let i = 0; i <= times; i++) {
-    randomMultiOutput.push(getRandom(50, 100));
-}
-console.log(randomMultiOutput);
+module.exports.getRandom = getRandom;

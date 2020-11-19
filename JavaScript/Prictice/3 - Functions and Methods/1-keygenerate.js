b65a304aa9f6b44bbfdb1277ce19b982118babcd
nikolaichub <chub.mykola@ukr.net>
2020-11-19 23:58:33 +0200

    1-keygenerate completed

--- JavaScript/Prictice/3 - Functions and Methods/1-keygenerate.js
+++ JavaScript/Prictice/3 - Functions and Methods/1-keygenerate.js
@@ -3,6 +3,16 @@
 // imports getRandom function
 const randomModule = require('./0-random');
 
-let a = randomModule.getRandom(0,50);
-console.log(a);
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const generateKey = (len, chars) => {
+    let key = '';
+    for (let i = 0; i < len; i++) {
+        let randomIndexOfChars = randomModule.getRandom(0,chars.length);
+        key += `${chars[randomIndexOfChars]}`;
+    };
 
+    return key;
+};
+
+
+console.log(generateKey(9, characters));

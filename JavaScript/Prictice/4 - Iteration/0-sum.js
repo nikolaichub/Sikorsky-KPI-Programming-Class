a2fd263f1eb603f791f60a59e0ff115db3bdee26
nikolaichub <chub.mykola@ukr.net>
2020-11-26 23:58:22 +0200

    summing arrays element via reduce, map, for, for..of, while

--- JavaScript/Prictice/4 - Iteration/0-sum.js
+++ JavaScript/Prictice/4 - Iteration/0-sum.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const a = sum(1, 2, 3) // 6
+const b = sum(0) // 0
+const c = sum() // 0
+const d = sum(1, -1, 1) // 1
+const e = sum(10, -1, -1, -1) // 7
+const f = sum(10, 400, -1, -1) // 408
+
+// via reduce
+function sum(...args) {
+    return args.reduce((a,b) => a + b, 0);
+}
+
+// via map
+function sum(...args) {
+    let sum = 0;
+    args.map( element => sum += element);
+    return sum;
+}
+
+// via for
+function sum(...args) {
+    let sum = 0;
+    for(let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+
+// via for..of
+function sum(...args) {
+    let sum = 0;
+    for (const i of args) {
+        sum += i;
+    }
+    return sum;
+}
+
+// via while
+function sum(...args) {
+    let sum = 0, 
+        i = 0;
+    while (i < args.length) {
+        sum += args[i];
+        i++;
+    }
+    return sum;
+}
+
+console.log(a);

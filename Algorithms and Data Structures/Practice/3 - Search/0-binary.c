7648efd2d0050ac26af7409c712691a6e26be1e7
nikolaichub <chub.mykola@ukr.net>
2020-12-01 15:05:34 +0200

    binary search method

---
+++ Algorithms and Data Structures/Practice/3 - Search/0-binary.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define N 11
+
+void print_array(int a[]) {
+    for (int i = 0; i < N; ++i) {
+        printf("%3d", a[i]);
+    }
+    printf("\n");
+}
+
+int search_element(int x, int a[]){
+    int L = 0; // 0
+    int R = N; // 10
+    int mid;
+
+    while (L < R) {
+        mid = (L + R)/2;
+
+        if (x == a[mid]) {
+            printf("position of '%d' is %d", x, mid);
+            break;
+        } else if(x < a[mid]) {
+            R = mid - 1;
+        } else if(x > a[mid]) {
+            L = mid + 1;
+        }
+    };
+    
+    return 0;
+}
+
+
+int main(){
+
+    int arr[N];
+    int x = 11;
+
+    for (int i = 0; i < N; i++){
+        arr[i] = i+1;
+    }
+    print_array(arr);
+
+    search_element(x, arr);
+    
+
+    return 0;
+}

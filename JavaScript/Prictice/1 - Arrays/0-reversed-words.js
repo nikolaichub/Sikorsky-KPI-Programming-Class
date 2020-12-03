9efad37483c5341291a67dde8598521533b842f9
nikolaichub <chub.mykola@ukr.net>
2020-12-03 23:27:14 +0200

    exteneded version added

--- JavaScript/Prictice/1 - Arrays/0-reversed-words.js
+++ JavaScript/Prictice/1 - Arrays/0-reversed-words.js
@@ -2,10 +2,30 @@
 
 // reverse each word of a string
 const original = 'Welcome to the club body!';
-const reversed = revWordsShort(original);
+const reversed = reverseWords(original);
+
+// extended version
+function reverseWords(string) {
+    // splits a string to an array
+    let splittedArr = string.split(' ');
+    let reversedArr = [];
+    splittedArr.forEach(word => {
+        // splits word into letters
+        let splittedWord = word.split('');
+
+        // reverses letters
+        let reversedWord = splittedWord.reverse();
+
+        // adds to final array connected word
+        reversedArr.push(reversedWord.join(''));
+    });
+
+    // connects all pieces
+    return reversedArr.join(' ');
+}
 
 // short version
-function revWordsShort(string) {
+function reverseWordsShort(string) {
     return string.split(' ').map(word => word.split('').reverse().reduce((prevWord, nexWord) => prevWord + nexWord)).join(' ');
 }
 

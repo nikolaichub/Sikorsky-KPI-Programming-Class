117dd6c567a79c301e2b483a990ec77329f51e08
nikolaichub <chub.mykola@ukr.net>
2020-12-03 22:04:44 +0200

    Prictice folder renamed to Labs

--- JavaScript/Labs/6 - Composition/0-pipe.js
+++ JavaScript/Labs/6 - Composition/0-pipe.js
@@ -4,8 +4,13 @@ const inc = x => ++x;
 const twice = x => x * 2;
 const cube = x => x ** 3;
 
-const f = pipe(inc, twice, cube);
 
-function f(params) {
-    
+// doest work.
+const f = pipe(inc, twice, cube);
+const x = f(5);
+
+function pipe(...params) {
+    console.log(params);
 }
+
+console.log(x);

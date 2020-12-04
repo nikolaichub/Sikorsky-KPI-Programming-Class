02d0a1239c39c52d6326aed66c6eb5b592780a30
nikolaichub <chub.mykola@ukr.net>
2020-12-05 00:46:07 +0200

    very odd Partial Application example added

---
+++ JavaScript/Prictice/Partial Application/0-coffee.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const coffee = (volume, strength) =>
+  `Coffee volume: ${volume}ml, strength: ${strength}`;
+
+const defineCoffeeType = volume => strength => coffee(volume, strength);
+
+// Use function defineCoffeeType to define new coffee types.
+// Define coffee type espresso which volume should equal 50ml.
+// Define coffee type americano which volume should equal 150ml.
+
+const espresso = defineCoffeeType(50)('espresso');
+const americano = defineCoffeeType(150);
+
+console.log(espresso);
+console.log(americano('americano'));

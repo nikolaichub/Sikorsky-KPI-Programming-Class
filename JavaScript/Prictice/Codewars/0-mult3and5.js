e2b439d059d54bfb89f5fb59289785311d126414
nikolaichub <chub.mykola@ukr.net>
2020-12-08 01:20:01 +0200

    folder for codewars tasks added

---
+++ JavaScript/Prictice/Codewars/0-mult3and5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function solution(number){
+    let sum = 0;
+
+    for (let i = 1; i < number; ++i) {
+        if ((i %3 == 0) || (i %5 == 0) ) {
+            console.log(i);
+            sum += i;
+        };
+    };
+
+    return sum;
+}
+
+console.log(solution(10));

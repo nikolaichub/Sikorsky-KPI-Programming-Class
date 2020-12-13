1c02452353d7b85bfe5638b3fb59f5d972d7f1c4
nikolaichub <chub.mykola@ukr.net>
2020-12-13 17:30:35 +0200

    seq updated

--- JavaScript/Labs/5 - Closure/0-seq.js
+++ JavaScript/Labs/5 - Closure/0-seq.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// seq will be here
+const seq = fX => gX => {
+    if (typeof gX === 'number') {
+        return fX(gX);
+    } else {
+        return seq(x => fX(gX(x)));
+    };
+};
+
+//const seq = f => g => (typeof g === 'number' ? f(g) : seq(x => f(g(x))));
+
+console.log(seq(x => x + 7)(x => x * 2)(5));

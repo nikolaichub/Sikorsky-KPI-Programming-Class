17b7de374a66395aa427055523528c9a74c87aae
nikolaichub <chub.mykola@ukr.net>
2020-12-15 13:37:56 +0200

    pipe finished

--- JavaScript/Labs/6 - Composition/0-pipe.js
+++ JavaScript/Labs/6 - Composition/0-pipe.js
@@ -4,13 +4,29 @@ const inc = x => ++x;
 const twice = x => x * 2;
 const cube = x => x ** 3;
 
+const pipe = (...params) => argument => {
+    // checks if parametr is a function
+    const isParamFunc = params.findIndex(elem => typeof elem != 'function') == -1;
+    // checks for parameters
+    const isParamLength = params.length != 0;
+    // checks if argument is a number
+    const isArgNumber = typeof argument == 'number';
 
-// doest work.
+    if (isParamFunc && isParamLength && isArgNumber) {
+        let result = argument;
+        for (const iterator of params) {
+            result = iterator(result);
+        }
+        return result;
+    } else {
+        return undefined;
+    }
+};
+
 const f = pipe(inc, twice, cube);
 const x = f(5);
 
-function pipe(...params) {
-    console.log(params);
-}
+const f1 = pipe(inc, inc);
+const x1 = f1(7);
 
-console.log(x);
+console.dir({x, x1});

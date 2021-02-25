62dcabf3214c3609667d27c161cd22d303b22279
nikolaichub <chub.mykola@ukr.net>
2021-02-25 18:29:43 +0200

    float replaced with double

--- Algorithms and Data Structures/Lab 6/0-rec-up.c
+++ Algorithms and Data Structures/Lab 6/0-rec-up.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-float c_ln(float x, float i, float* sum) {
-    float k = 0;
-    printf("k = %f sum = %f i = %f\n", k, *sum, i);
+double c_ln(double x, double i, double* sum) {
+    double k = 0;
+    printf("k = %lf sum = %lf i = %lf\n", k, *sum, i);
     if (i == 1) {
         return *sum = (x - 1) / x;
     }
     k = c_ln(x, i - 1, sum) * (i - 1) * (x - 1) / (i * x);
     *sum += k;
-    printf("k = %f sum = %f i = %f\n", k, *sum, i);
+    printf("k = %lf sum = %lf i = %lf\n", k, *sum, i);
     return k;
 }
 
 int main() {
-    float real, x, i, sum = 0;
+    double real, x, i, sum = 0;
     printf("x = ");
-    scanf("%f", &x);
+    scanf_s("%lf", &x);
     printf("i = ");
-    scanf("%f", &i);
+    scanf_s("%lf", &i);
 
     real = log(x);
     c_ln(x, i, &sum);
-    printf("Calculated value of ln(%f) = %f\n", x, sum);
-    printf("Real value of ln(%.1f) = %f", x, real);
+    printf("Calculated value of ln(%lf) = %lf\n", x, sum);
+    printf("Real value of ln(%.1lf) = %lf", x, real);
 
     return 0;
 }
